3d1a62d3d1b411f9fd133d0931c482ca9c91e475
lexy <lexyblazy@gmail.com>
2017-07-20 12:51:12 +0100

    updated package.json file and server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const app = express();
 const path = require('path');
 const publicPath = path.join(__dirname,'../public');
 
+var PORT = process.env.PORT || 3000;
+var IP = process.env.IP;
 app.use(express.static(publicPath));
 
 app.get('/',(req,res)=>{
 //    res.render('index.html');
 })
 
-app.listen(3000,()=>{
+app.listen(PORT,IP,()=>{
     console.log('Server is up and running on port 3000');
 })
