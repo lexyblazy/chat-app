72aa616a744be46888b07b9dcb4a7c654f68da76
lexy <lexyblazy@gmail.com>
2017-07-26 14:54:57 +0100

    added location button and location script to index files

--- public/js/index.js
+++ public/js/index.js
@@ -26,3 +26,17 @@ $('#message-form').on('submit', function(e){
     $('#message-text').val('');
     
 })
+
+$('#location-button').on('click',function(){
+    
+    if(!navigator.geolocation){
+        return alert('Your browser does not support this feature')
+    }
+    
+    navigator.geolocation.getCurrentPosition(function (positon){
+        console.log(positon)
+    },function(err){
+        console.log(err);
+        console.log('cannnot find position')
+    })
+})
