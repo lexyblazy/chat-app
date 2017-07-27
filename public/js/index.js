9c970194b0bf5ff44f7095906a27b2adbc81ed06
lexy <lexyblazy@gmail.com>
2017-07-27 07:59:08 +0100

    added a good ux to the index html page via index js

--- public/js/index.js
+++ public/js/index.js
@@ -26,32 +26,40 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server');
 })
 
+var messageTextBox = $('#message-text');
+
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
     socket.emit('message', {
         from: 'User',
-        text: $('#message-text').val()
+        text: messageTextBox.val()
     }, function () {
-
+        
+        messageTextBox.val('');
     });
-    $('#message-text').val('');
+   
 
 })
 
-$('#location-button').on('click', function () {
+var locationButton =  $('#location-button');
+
+locationButton.on('click', function () {
 
     if (!navigator.geolocation) {
         return alert('Your browser does not support this feature')
     }
+    
+    locationButton.attr('disabled','disabled').text('Sending Location...')
 
     navigator.geolocation.getCurrentPosition(function (position) {
-        console.log(position.coords);
+        
+        location.removeAttr('disabled').texy('Send Location');
         socket.emit('createLocationMessage', {
             longitude: position.coords.longitude,
             latitude: position.coords.latitude
         })
     }, function (err) {
         console.log(err);
-        console.log('cannnot find position')
+        location.removeAttr('disabled').text('Send Location');
     })
 })
